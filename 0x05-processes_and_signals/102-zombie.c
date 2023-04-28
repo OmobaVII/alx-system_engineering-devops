7773707c2333e80be1f111621b1cede10adc77ff
OmobaVII <omoba.career@gmail.com>
2023-04-28 13:00:15 -0700

    Zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - a program that creates 5 zombie processes
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int a;
+	pid_t pid;
+
+	for (a = 0; a <= 4; a++)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		printf("Zombie process created, PID: %d\n", pid);
+	}
+	return (infinite_while());
+}
+/**
+ * infinite_while - an infinite while loop
+ * Return: 0 for success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
