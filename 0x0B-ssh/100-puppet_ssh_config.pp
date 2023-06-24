c2b72867c6c9160acca8576b45f2a31744539489
OmobaVII <omoba.career@gmail.com>
2023-06-24 03:29:45 -0700

    Client configuration fil with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Configuring SSH configuration file to connect to a server without a password
+# SSH client configuration to use private key ~/.ssh/school
+# SSH client configuration to refuse authenticate using a password
+
+file { '/root/.ssh/config':
+    ensure  => file,
+    path    => '/root/.ssh/config',
+    content => "Host 18.235.234.111\n\t PasswordAuthentication no\n\t IdentityFile ~/.ssh/school",
+    mode    => '0600',
+}
