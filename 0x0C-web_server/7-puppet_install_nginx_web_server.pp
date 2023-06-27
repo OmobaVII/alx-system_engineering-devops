5ae01b60bb75d8ad313567c2976379eba73db25a
OmobaVII <omoba.career@gmail.com>
2023-06-27 13:35:35 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,4 +4,31 @@ exec { 'update packages':
 package { 'nginx':
     ensure => 'installed',
 }
+file { '/var/www/html/index.html':
+    content => "Hello World!\n",
+}
+file { 'Nginx config file':
+    ensure  => file,
+    path    => '/etc/nginx/sites-enabled/default',
+    content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
+        # SSL configuration
+        #
+        # listen 443 ssl default_server;
+        # listen [::]:443 ssl default_server;
+        #
+        # Note: You should disable gzip for SSL traffic.
+        # See: https://bugs.debian.org/773332
+        #
+        location /redirect_me {
+                 return 301 https://linkedin.com/in/sanni-omoba;
+        }
+}
+",
+}
+exec { 'restart nginx':
+    command => 'sudo /etc/init.d/nginx restart',
+}
