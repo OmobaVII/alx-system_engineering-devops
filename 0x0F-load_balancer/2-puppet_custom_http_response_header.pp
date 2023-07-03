f130a47571e92a07847631fccb4020e2a434eebc
OmobaVII <omoba.career@gmail.com>
2023-07-03 14:49:35 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,6 +3,12 @@ exec { 'update packages':
     command => 'apt-get -y update',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+exec {'install Nginx':
+    provider => posix,
+    command  => 'sudo apt-get -y install nginx',
+    path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    before   => Exec['add_header'],
+}
 package { 'nginx':
     ensure => 'installed',
 }
@@ -10,6 +16,7 @@ exec { 'add_header':
     provider => posix,
     command  => "sudo sed -i '23a\\\tadd_header X-Served-By \"\$HOSTNAME\";' /etc/nginx/sites-available/default",
     path     => '/usr/bin:/usr/sbin:/bin:/sbin',
+    before   => Exec['restart nginx']
 }
 exec { 'restart nginx':
     command => 'sudo /etc/init.d/nginx restart',
