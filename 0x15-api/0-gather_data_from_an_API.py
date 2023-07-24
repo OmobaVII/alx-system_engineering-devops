55629b8775ed799b47019651f36fcab9123d0d93
OmobaVII <omoba.career@gmail.com>
2023-07-24 06:18:48 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Uses READ API for a given employee ID,
+Returns information about the employee TODO list
+progress
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """prevents code form executing when imported"""
+    all_users = requests.get("https://jsonplaceholder.typicode.com/users")
+    for user in all_users.json():
+        if user.get('id') == int(argv[1]):
+            Username = (user.get("name"))
+            break
+    Completed = 0
+    Total = 0
+    Title = []
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for task in tasks.json():
+        if task.get("userId") == int(argv[1]):
+            Total += 1
+            if task.get("completed") is True:
+                Completed += 1
+                Title.append(task.get("title"))
+    print("Employee {} is done with taks({}/{}):".format(Username, Completed,
+                                                         Total))
+    for task in Title:
+        print("\t{}".format(task))
